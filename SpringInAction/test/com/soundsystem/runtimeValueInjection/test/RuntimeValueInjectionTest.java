ac65b4d9f2db5be7625c80ad7a556e2216212ed6
Uğur Yeter <ytr.ugur@gmail.com>
2016-12-13 12:50:20 +0200

    Runtime Injection Sample implementation.

---
+++ SpringInAction/test/com/soundsystem/runtimeValueInjection/test/RuntimeValueInjectionTest.java
@@ -0,0 +1,26 @@
+package com.soundsystem.runtimeValueInjection.test;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.soundsystem.runtimeValueInjection.BlankDisc;
+import com.soundsystem.runtimeValueInjection.ExpressiveConfig;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes=ExpressiveConfig.class)
+public class RuntimeValueInjectionTest {
+
+	@Autowired
+	BlankDisc blankDisc;
+	
+	@Test
+	public void testRuntimeInjection(){
+		assertEquals("The Beatles", blankDisc.getArtist());
+		
+	}
+}
